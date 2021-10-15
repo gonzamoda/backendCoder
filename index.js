78b1cdcf54e26bebc8c21c8420dc239678548c74
Gonzalo Modarelli <gonzalomodarelli@Air-de-Gonzalo.fibertel.com.ar>
2021-10-14 23:11:57 -0300

    desafio servidor con express

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require("express");
-const moment = require("moment");
-const PORT = 3000;
+const PORT = 8080;
 let app = express();
-let counterPage = 0;
+const fs = require("fs");
+
+class Contenedor {
+  constructor(archivo) {
+    this.archivo = archivo;
+  }
+
+  getAll() {
+    app.get("/productos", (req, res, next) => {
+      let todosLosProductos = JSON.parse(
+        fs.readFileSync(archivoProductos.archivo, "utf-8")
+      );
+      res.send(todosLosProductos);
+    });
+  }
+
+  getRandom() {
+    app.get("/productoRandom", (req, res, next) => {
+      let todosLosProductos = JSON.parse(
+        fs.readFileSync(archivoProductos.archivo, "utf-8")
+      );
+      let productoRandom =
+        todosLosProductos[Math.floor(Math.random() * todosLosProductos.length)];
+      res.json(productoRandom);
+    });
+  }
+}
+
+const archivoProductos = new Contenedor("productos.txt");
+
+archivoProductos.getAll();
+archivoProductos.getRandom();
 
 app.get("/", (req, res, next) => {
   let response = `<h1 style="color:blue;font-family:'Roboto';font-size:35pt;">Bienvenidos al servidor express</h1>`;
   res.send(response);
-});
-
-app.get("/visitas", (req, res, next) => {
-  counterPage++;
-  res.send(`Esta pagina ha tenido ${counterPage} visitas`);
-});
-
-app.get("/fyh", (req, res, next) => {
-  res.json({ fyh: moment().format("YYYY-MM-DD HH:MM:SS").toString() });
 });
 
 app.listen(PORT, () => {
